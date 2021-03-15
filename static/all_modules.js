cdfd673a109876be5aca396fce722beca13594e9
Kevin Chalmers <kevin.chalmers@roehampton.ac.uk>
2021-03-15 11:19:03 +0000

    all_modules now gets data from backend.

--- static/all_modules.js
+++ static/all_modules.js
@@ -3,9 +3,7 @@
 var mainApp = angular.module("mainApp", []);
 
 mainApp.controller("moduleController", function($scope, $http) {
-    $scope.modules = [
-        new Module("000", "Test Module 1"),
-        new Module("001", "Test Module 2"),
-        new Module("002", "Test Module 3")
-    ];
+    $http.get("/modules").then(function(response) {
+        $scope.modules = response.data;
+    });
 });
