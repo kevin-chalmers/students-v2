993d07fc61e8d2c116b571d018415338eccc7d5d
Kevin Chalmers <kevin.chalmers@roehampton.ac.uk>
2021-03-15 10:36:22 +0000

    Added ng-repeat to all_modules.

--- static/all_modules.js
+++ static/all_modules.js
@@ -3,5 +3,9 @@
 var mainApp = angular.module("mainApp", []);
 
 mainApp.controller("moduleController", function($scope, $http) {
-    $scope.module = new Module("000", "Test");
+    $scope.modules = [
+        new Module("000", "Test Module 1"),
+        new Module("001", "Test Module 2"),
+        new Module("002", "Test Module 3")
+    ];
 });
