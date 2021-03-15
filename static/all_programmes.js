77989a8dd532198eaff471e2d6f2716f1c360348
Kevin Chalmers <kevin.chalmers@roehampton.ac.uk>
2021-03-15 11:21:44 +0000

    all_programmes gets data from backend

--- static/all_programmes.js
+++ static/all_programmes.js
@@ -3,8 +3,7 @@
 var mainApp = angular.module("mainApp", []);
 
 mainApp.controller("programmeController", function($scope, $http) {
-    $scope.programmes = [
-        new Programme("001", "Test Program 1"),
-        new Programme("002", "Test Program 2")
-    ];
+    $http.get("/programmes").then(function(response) {
+        $scope.programmes = response.data;
+    });
 });
