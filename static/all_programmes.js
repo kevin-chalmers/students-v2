38cfd529e267a23fa712782dc555103ef98496ec
Kevin Chalmers <kevin.chalmers@roehampton.ac.uk>
2021-03-15 12:21:41 +0000

    Added all sorts

--- static/all_programmes.js
+++ static/all_programmes.js
@@ -3,7 +3,16 @@
 var mainApp = angular.module("mainApp", []);
 
 mainApp.controller("programmeController", function($scope, $http) {
+    document.getElementById("selected").style.display = "none";
+
     $http.get("/programmes").then(function(response) {
         $scope.programmes = response.data;
     });
+
+    $scope.selectProgramme = function(code) {
+        $http.get("/programme/" + code).then(function(response) {
+            $scope.selectedProgramme = response.data;
+            document.getElementById("selected").style.display = "block";
+        });
+    };
 });
--- static/single_student.js
+++ static/single_student.js
@@ -4,12 +4,8 @@ var mainApp = angular.module("mainApp", []);
 
 mainApp.controller("studentController", function($scope, $http) {
     document.getElementById("selected").style.display = "none";
-    
-    $http.get("/students").then(function(response) {
-        $scope.students = response.data;
-    });
 
-    $scope.selectStudent = function(id) {
+    $scope.getStudent = function(id) {
         $http.get("/student/" + id).then(function(response) {
             $scope.selectedStudent = response.data;
             document.getElementById("selected").style.display = "block";
