b2a6dd4467bee02b2bc39ed092fcac6238423122
Kevin Chalmers <kevin.chalmers@roehampton.ac.uk>
2021-03-15 11:25:20 +0000

    all_students now talks to backend

--- static/all_students.js
+++ static/all_students.js
@@ -3,8 +3,7 @@
 var mainApp = angular.module("mainApp", []);
 
 mainApp.controller("studentController", function($scope, $http) {
-    $scope.students = [
-        new Student("001", "A", "Student", new Programme("001", "Test Programme 1")),
-        new Student("002", "Another", "Student", new Programme("002", "Test Programme 2"))
-    ];
+    $http.get("/students").then(function(response) {
+        $scope.students = response.data;
+    });
 });
