eeb3eec3049d5f27c94b00b1d6ec379f3ef26cf3
Kevin Chalmers <kevin.chalmers@roehampton.ac.uk>
2021-03-22 11:27:59 +0000

    Added delete student to front end

--- static/all_students.js
+++ static/all_students.js
@@ -14,5 +14,15 @@ mainApp.controller("studentController", function($scope, $http) {
             $scope.selectedStudent = response.data;
             document.getElementById("selected").style.display = "block";
         });
-    };  
+    };
+    
+    $scope.deleteStudent = function(id) {
+        // Send delete message to the server
+        $http.delete("/student/" + id).then(function(response) {
+            // Refresh the list of students
+            $http.get("/students").then(function(response) {
+                $scope.students = response.data;
+            });
+        });
+    };
 });
